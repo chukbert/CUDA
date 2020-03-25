deb65df008967bdbb6dde62698c36aca40bf0327
hanifadzkiya <13517120@std.stei.itb.ac.id>
2020-03-25 22:00:05 +0700

    Add dijkstra

--- main.cu
+++ main.cu
@@ -1,30 +1,69 @@
 #include <iostream>
 #include <math.h>
+
+void Dijkstra(int src);
 // Kernel function to add the elements of two arrays
 __global__
+void Dijkstra(int src) {
+    int dist[V + 5];
+	int sptSet[V + 5];
+
+	for (int i = 0; i < V; i++) 
+		dist[i] = INT_MAX, sptSet[i] = 0; 
+
+	dist[src] = 0; 
+
+	for (int count = 0; count < V - 1; count++) { 
+		int min = INT_MAX, min_index; 
+        for (int v = 0; v < V; v++) 
+            if (sptSet[v] == 0 && dist[v] <= min) 
+                min = dist[v], min_index = v;
+        int u = min_index; 
+ 
+		sptSet[u] = 1; 
+
+		for (int v = 0; v < V; v++) 
+			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
+				&& dist[u] + graph[u][v] < dist[v]) 
+				dist[v] = dist[u] + graph[u][v]; 
+	}
+
+	for (int i=0; i<V; i++) {
+        hasil_gabung[src][i] = dist[i];
+    }
+}
+
+
 void add(int n, float *x, float *y)
 {
   for (int i = 0; i < n; i++)
     y[i] = x[i] + y[i];
 }
 
+
 int main(void)
 {
   int N = 1<<20;
   float *x, *y;
 
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+    // initialize map
+    srand(13517093);
+    for(int i = 0;i<V;i++) {
+        graph[i][i] = 0;
+        for(int j = i+1;j<V;j++) {
+            graph[i][j] = rand() % 23;
+            if(graph[i][j] == 0) graph[i][j] = 1;
+            graph[j][i] = graph[i][j];
+        }
+    }
+
 
   // Run kernel on 1M elements on the GPU
-  add<<<1, 1>>>(N, x, y);
+    dijkstra<<<1, 1>>>(N, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
